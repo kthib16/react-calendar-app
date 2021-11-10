dc2a7b23272eeb926185a6b0240509819d0700d8
Kevin Thibodeaux <kthib16@gmail.com>
2021-11-10 09:47:30 -0500

    Adds app.js file and event list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Switch, Route, NavLink } from 'react-router-dom';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs} from 'firebase/firestore';
+import EventItem from './pages/EventItem';
 
-function App() {
+const firebaseConfig = {
+  apiKey: "AIzaSyCBerhVvYqX_oITGaqgzbYhtT01AoprpzE",
+  authDomain: "jsr-914-3a79b.firebaseapp.com",
+  projectId: "jsr-914-3a79b",
+  storageBucket: "jsr-914-3a79b.appspot.com",
+  messagingSenderId: "573368560534",
+  appId: "1:573368560534:web:5c4bb34ecb946546d20bb7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db= getFirestore(app);
+
+export default class App extends React.Component {
+state = {
+  events: []
+}
+
+componentDidMount() {
+  this.getEvents();
+}
+
+getEvents = async () => {
+  const eventsCollection = collection(db, 'Events');
+  const events = await getDocs(eventsCollection);
+  const eventsArr = [];
+  events.forEach(individualEvent => {
+    const eachEvent = {
+      id: individualEvent.id,
+        date: individualEvent.data().date,
+        month: individualEvent.data().month,
+        year: individualEvent.data().year,
+        eventName: individualEvent.data().eventName,
+        guestsAttending: individualEvent.data().guestsAttending,
+        isGoing: individualEvent.data().isGoing,
+        locationCity: individualEvent.data().locationCity,
+        locationState: individualEvent.data().locationState
+    }
+    eventsArr.push(eachEvent)
+  })
+
+  this.setState({
+    events: eventsArr
+  })
+}
+
+render(){
   return (
     <div className="App">
       <h4>My calendar</h4>
-
+      <EventItem eventProps={this.state.events}/>
     </div>
   );
 }
-
-export default App;
+}
---
+++ src/pages/EventItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const EventItem = (props) => {
+  const eventList = props.eventProps.map(eventItem => {
+    return(
+      <li key={eventItem.id} className='list-group-item'>
+        <div className='list-inline-item'>
+          {eventItem.eventName}
+        </div>
+        <div className='list-inline-item float-right'>
+          <i className='fas fa-edit'></i>
+          <i className='fas fa-trash'></i>
+        </div>
+      </li>
+    )
+  })
+
+  return(
+    <ul className='list-group'>
+      {eventList}
+    </ul>
+  )
+}
+
+export default EventItem;
